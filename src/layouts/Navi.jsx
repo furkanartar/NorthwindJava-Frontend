46d3a11deade02be6955a743ac85bbaa0972dce9
furkanartar <asosyalgenctvtr@gmail.com>
2021-06-18 10:44:06 +0300

    Login, Component, services and Redux structure has been added.

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import CartSummary from "./CartSummary";
-import { Button, Container, Dropdown, Menu } from "semantic-ui-react";
+import { Container, Menu } from "semantic-ui-react";
+import SignedOut from "./SignedOut";
+import SignedIn from "./SignedIn";
+import { useHistory } from "react-router";
+import { useSelector } from 'react-redux';
 
 export default function Navi() {
+  const {cartItems} = useSelector(state => state.cart)
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const history = useHistory()
+  function handleSignOut() {
+    setIsAuthenticated(false)
+    history.push("/")
+  }
+
+  function handleSignIn() {
+    setIsAuthenticated(true)
+  }
+
   return (
     <div>
       <Menu inverted fixed="top">
@@ -11,15 +27,12 @@ export default function Navi() {
           <Menu.Item name="messages" />
 
           <Menu.Menu position="right">
-          <CartSummary />
-          
-            <Menu.Item>
-              <Button primary>Sign Up</Button>
-            </Menu.Item>
+            {cartItems.length>0&&<CartSummary/>}
+            {isAuthenticated?<SignedIn signOut={handleSignOut} bisey="1"/>
+            :<SignedOut signIn={handleSignIn}/>}  
           </Menu.Menu>
         </Container>
       </Menu>
-
     </div>
   );
 }
